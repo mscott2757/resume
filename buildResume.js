6ccc8172ad10a871785535b9c196efb76ab4bf91
Mason Scott <mscott2757@gmail.com>
2024-06-17 23:34:54 -0700

    skills

--- buildResume.js
+++ buildResume.js
@@ -1,5 +1,4 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const exec = require("child_process").exec;
 const path = require("path");
 const fs = require("fs");
 
@@ -24,21 +23,33 @@ const transforms = {
       }, [])
       .join("\n");
   },
+  skills: (data) => {
+    const lines = [
+      '\\begin{tabular}{L{4.8cm} l}',
+      data.map(({ name, items }) => `\\textbf{${name}} & ${items.join(', ')}\\\\`).join('\n\\\\[-1em]\n'),
+      '\\end{tabular}',
+    ]
+
+    return lines.join('\n');
+  }
 };
 
 const main = async () => {
   let template = fs.readFileSync(path.join(__dirname, "template.tex"), {
     encoding: "utf8",
   });
-  const content = JSON.parse(fs.readFileSync(path.join(__dirname, 'content.json')))
+  const content = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "content.json"))
+  );
 
   for (const [key, genContent] of Object.entries(transforms)) {
     template = template.replace(`{{${key}}}`, genContent(content[key]));
   }
 
   fs.writeFileSync(path.join(__dirname, "resume.tex"), template);
-  await exec("xelatex resume.tex && open resume.pdf");
-  await exec('rm resume.tex && open resume.pdf');
+  exec(
+    "xelatex resume.tex && open resume.pdf && rm resume.tex"
+  ).stdout.pipe(process.stdout);
 };
 
 main();
